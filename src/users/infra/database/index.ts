e716534880119128fe1221e45614b30e6f63e079
André Rodrigues <andre.rodrigues98@outlook.com>
2024-01-08 09:47:07 -0300

    feat: adding abstraction to uuid generator

--- src/users/infra/database/index.ts
+++ src/users/infra/database/index.ts
@@ -1,15 +1,15 @@
-import { randomUUID } from "node:crypto";
+import * as uuidHelper from "../../../app/infra/helpers/UuidHelper.helper";
 
 export const users = [
   {
-    id: randomUUID(),
+    id: uuidHelper.generate(),
     name: "teste",
     email: "some email",
     profile_id: 1,
     status: "BLOCKED",
   },
   {
-    id: randomUUID(),
+    id: uuidHelper.generate(),
     name: "teste 2",
     email: "some email 2",
     salary_real: 1002,
@@ -17,14 +17,14 @@ export const users = [
     status: "INACTIVE",
   },
   {
-    id: randomUUID(),
+    id: uuidHelper.generate(),
     name: "teste 3",
     email: "some email 3",
     profile_id: 2,
     status: "ACTIVE",
   },
   {
-    id: randomUUID(),
+    id: uuidHelper.generate(),
     name: "teste 4",
     email: "some email 4",
     profile_id: 2,
