75d2e1b90b8c361abe5bafae8d0e345b189e15dd
André Rodrigues <andre.rodrigues98@outlook.com>
2024-01-08 09:08:12 -0300

    fix: adding uuid as default id

--- src/users/infra/database/index.ts
+++ src/users/infra/database/index.ts
@@ -1,13 +1,15 @@
+import { randomUUID } from "node:crypto";
+
 export const users = {
   1: {
-    id: "1",
+    id: randomUUID(),
     name: "teste",
     email: "some email",
     profile_id: 1,
     status: "BLOCKED",
   },
   2: {
-    id: "2",
+    id: randomUUID(),
     name: "teste 2",
     email: "some email 2",
     salary_real: 1002,
@@ -15,14 +17,14 @@ export const users = {
     status: "INACTIVE",
   },
   3: {
-    id: "3",
+    id: randomUUID(),
     name: "teste 3",
     email: "some email 3",
     profile_id: 2,
     status: "ACTIVE",
   },
   4: {
-    id: "4",
+    id: randomUUID(),
     name: "teste 4",
     email: "some email 4",
     profile_id: 2,
